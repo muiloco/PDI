f6a9d28db754120b4787cca38277541f14c98a9d
muiloco <muiloco17@gmail.com>
2019-08-26 12:12:40 -0300

    Algoritmo de Operacoes sobre a imagem normalizacao

--- imageOperation.m
+++ imageOperation.m
@@ -1,6 +1,22 @@
 a = imread('lena.jpg');
 b = rgb2gray(a); 
-imshow(b);
 c = b * 2;
+d = c * 2;
+[x,y] = size(c);
+maximo = c(1,1);
+minimo = c(1,1);
+for i = 1:x
+    for j = 1:y
+        if c(i,j) < minimo
+            minimo = c(i,j);
+        end
+        if c(i,j) > maximo
+            maximo = c(i,j);
+        end
+    end
+end
+
+final = (255/(maximo-minimo))*(c-minimo);
+imshow(final);
 figure;
-imshow(c);
+imshow(c)
